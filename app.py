9499b1cdca9e065c974b21d461ec36ac62042149
I.F <131729144+FukumotoIkuma@users.noreply.github.com>
2024-08-09 18:43:17 +0900

    webhookに対してメッセージを送信できるコードを作成

--- app.py
+++ app.py
@@ -1,80 +1,14 @@
-from os import PathLike
 from flask import Flask, request,jsonify,make_response
 from pprint import pformat
 from system import create_hmac_sha256
 import os
-import pandas as pd
-import schedule#require
-import time
-from datetime import datetime, timedelta
-import threading
 
-class CallManager:
-    """call_idと電話番号を管理するクラス
-    """
-    def __init__(self):
-        self.CALL_ID = "call_id"
-        self.PHONE_NUMBER = "phone_number"
-        self.CREATED_AT = "created_at"
-        self.df = pd.DataFrame(columns=[self.CALL_ID, self.PHONE_NUMBER, self.CREATED_AT])
-
-    def add_record(self, call_id:str, phone_number:str):
-        """レコードを追加する
-
-        Args:
-            call_id (str): webhookに含まれるcall_id
-            phone_number (str): call_idに対応するphone_number
-        """
-
-        created_at = datetime.now()
-        new_record = pd.DataFrame({'call_id': [call_id], 'phone_number': [phone_number], 'created_at': [created_at]})
-        self.df = pd.concat([self.df, new_record], ignore_index=True)
-        print(f"Added record: {new_record.iloc[0].to_dict()}")
-
-
-    def get_phone_number(self, call_id:str):
-        """call_idからphone_numberを取得する
-
-        Args:
-            call_id (str): webhookに含まれるcall_id
-
-        Returns:
-            str: 電話番号
-            None: call_idが存在しない
-        """
-        record = self.df[self.df[self.CALL_ID] == call_id]
-        if not record.empty:
-            return record.iloc[0][self.PHONE_NUMBER]
-        else:
-            return None
-
-    def start_cleanup_schedule(self, interval_minutes=30):
-        self.interval_minutes = interval_minutes
-        cleanup_thread = threading.Thread(target=self._start_cleanup_schedule)
-        cleanup_thread.daemon = True
-        cleanup_thread.start()
-
-    def _start_cleanup_schedule(self):
-        schedule.every(self.interval_minutes).minutes.do(self._delete_old_records)
-        while True:
-            schedule.run_pending()
-            time.sleep(60*10)
-
-    def _delete_old_records(self):
-        current_time = datetime.now()
-        threshold_time = current_time - timedelta(minutes=30)
-        self.df = self.df[self.df[self.CREATED_AT] > threshold_time]
-        print("Old records deleted")
-
-# インスタンスの作成
-call_manager = CallManager()
-
-# 定期的に古いレコードを削除するスケジュールを開始（バックグラウンドで実行）
-call_manager.start_cleanup_schedule()
+from send_sms import ZoomAPI,send_sms
 
 
 app = Flask(__name__)
 show_text = ["no webhook"]
+zoom_api = ZoomAPI()
 
 
 #環境変数の読み込み
@@ -87,7 +21,7 @@ except:#鯖
 
 @app.route("/",methods = ['GET'])
 def home():
-    return call_manager.get_phone_number(1)
+    return "hello world!"
 
 @app.route("/webhook",methods = ["POST"])
 def webhook():
@@ -110,17 +44,19 @@ def webhook():
         res =jsonify(res_json)
         res.status_code = 200
         return res
-    
-    elif event == "phone.caller_connected":
-        obj = payload["object"]
-        call_id = obj["call_id"]
-        phone_number = obj["callee"]["phone_number"]
-        call_manager.add_record(call_id,phone_number)
 
     elif event == "phone.callee_ended":
         call_id = payload["object"]["call_id"]
-        phone_number = call_manager.get_phone_number(call_id=call_id)
-        # TODO SMS送信処理
+        phone_number,extension_numbers = zoom_api.extract_SMS_info(call_id)
+        print(f"extension_numbers: {extension_numbers}")
+
+        #TODO 本番環境で可変にすべき
+        if "210" in extension_numbers:
+            try:
+                send_sms(phone_number=phone_number,message="test")
+                print("completely sended msg")
+            except Exception as e:
+                print(e)
 
     return make_response("",200)
 
---
+++ send_sms.py
@@ -0,0 +1,205 @@
+import base64
+import requests
+
+from pprint import pprint
+import time
+from datetime import datetime, timedelta
+import boto3#requirement
+import os
+
+try:#ローカル
+    from SMS_SECRET_INFO import ZOOM_CLIENT_ID,ZOOM_CLIENT_SECRET,ZOOM_ACCOUNT_ID,AWS_ACCESS_KEY,AWS_SECRET_KEY
+except:#鯖
+    ZOOM_CLIENT_ID      =  os.getenv('ZOOM_CLIENT_ID')
+    ZOOM_CLIENT_SECRET  =  os.getenv('ZOOM_CLIENT_SECRET')
+    ZOOM_ACCOUNT_ID     =  os.getenv('ZOOM_ACCOUNT_ID')
+    AWS_ACCESS_KEY      =  os.getenv('AWS_ACCESS_KEY')
+    AWS_SECRET_KEY      =  os.getenv('AWS_SECRET_KEY')
+
+
+class ZoomTokenMaker:
+    def __init__(self):
+        self._token = None
+        self.token_creation_time = None
+
+    def generate_token(self):
+        # クライアントIDとクライアントシークレットをbase64形式でエンコード
+        client_credentials = f"{ZOOM_CLIENT_ID}:{ZOOM_CLIENT_SECRET}"
+        encoded_credentials = base64.b64encode(client_credentials.encode()).decode()
+
+        # ヘッダーの設定
+        headers = {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Authorization": f"Basic {encoded_credentials}"
+        }
+
+        # ボディの設定
+        body = {
+            "grant_type": "account_credentials",
+            "account_id": ZOOM_ACCOUNT_ID
+        }
+
+        # トークンAPIのエンドポイント
+        token_url = "https://zoom.us/oauth/token"
+
+        # POSTリクエストの送信
+        response = requests.post(token_url, headers=headers, data=body)
+
+        # レスポンスの確認
+        if response.status_code == 200:
+            access_token = response.json().get("access_token")
+            return access_token
+        else:
+            raise Exception("Coldn't get access token")
+        
+    @property
+    def token(self):
+        """有効なTokenを返す
+
+        Returns:
+            _type_: _description_
+        """
+        if self._is_token_expired():
+            self._token = self.generate_token()
+        return self._token
+
+    def _is_token_expired(self):
+        if self.token_creation_time is None:
+            return True
+        return datetime.now() - self.token_creation_time > timedelta(minutes=50)
+
+class ZoomAPI(object):
+    def __init__(self) -> None:
+        self.token_mng = ZoomTokenMaker()
+        self.base_url ="https://api.zoom.us/v2"
+        self.call_history_url = self.base_url+"/phone/call_history/{callLogId}"
+
+    def extract_keys_from_segments(self,segments, *args):
+        """
+        特定のキーを抽出したセグメントの新しいリストを作成する関数。
+
+        Parameters:
+            segments (list): 元のセグメントのリスト。
+            *args: 抽出したいキー。
+
+        Returns:
+            list: 抽出されたキーに基づく新しいセグメントのリスト。
+        """
+        extracted_segments = [{key: segment.get(key) for key in args}for segment in  segments]
+
+        
+        return extracted_segments
+
+    def remove_duplicate_segments(self,segments):
+        """
+        重複したセグメントを取り除く関数。
+
+        Parameters:
+            segments (list): 元のセグメントのリスト。
+
+        Returns:
+            list: 重複が取り除かれたセグメントのリスト。
+        """
+        unique_segments = []
+        seen = set()
+
+        for segment in segments:
+            # セグメントの各項目をタプルにして一意性を確保
+            segment_tuple = tuple(sorted(segment.items()))
+            
+            if segment_tuple not in seen:
+                seen.add(segment_tuple)
+                unique_segments.append(segment)
+
+        return unique_segments
+
+    def extract_SMS_info(self,call_id:str):
+        """APIによりcall_idからSMS送信が必要か判断するための取得する
+
+        Args:
+            call_id (str): webhookで得られるcall_id
+
+        Returns:
+            str: 電話番号(+813012341234/国際電話形式)
+            set: 経由した内線番号のリスト
+        """
+        
+        # ヘッダーの設定
+        headers = {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Authorization": f"Bearer {self.token_mng.token}"
+        }
+
+        
+
+        # call historyからcallLogIdを取得する
+        # webhookレスポンスにコールログのIDが存在するが、そのIDと現在取得しているIDは
+        # 別物らしい（スクリプト作成時点）。そのうち統合されるかも？
+        # https://developers.zoom.us/docs/zoom-phone/understanding-call-history/
+        
+        # キーワードにcall_idを指定することで確実に絞り込み
+        # TODO HEAVYなので、40 リクエスト/秒、60000/dayまでである。少し数を減らす工夫が必要そう
+        params ={
+            "keyword":call_id
+        }
+        history_response = requests.get(
+            self.call_history_url.format(callLogId=""),
+            headers=headers,params=params
+        )
+        if history_response.status_code!=200:
+            return#TODO errorハンドリング
+        history = history_response.json()["call_logs"]
+        # keywordがマッチする値はcall_idで絞り込んでるとは言え不確定なので、手動で絞り込みも行う
+        call_log_id = [his for his in history if his["call_id"]==call_id][0]["id"]
+
+        # call_log_idからcall_logを取得し、整形する
+        # LIGHTなので、制限は80 リクエスト/秒。気にせずアクセスして良い。
+        callLog_response = requests.get(self.call_history_url.format(callLogId = call_log_id),headers=headers)
+        phone_number = callLog_response.json()["callee_did_number"]
+        segments = callLog_response.json()["call_path"]
+        
+        # ログ変換にはこれらの処理を利用できそう。メモとして残す
+        # segments = extract_keys_from_segments(segments,"event","result","operator_ext_number","callee_ext_number","operator_ext_Type","callee_ext_type","operator_name","caller_name","callee_name","press_key")
+        # segments = remove_duplicate_segments(segments)
+
+        segments = self.extract_keys_from_segments(segments,"operator_ext_number")
+        extension_numbers = set([dic.get("operator_ext_number") for dic in segments])
+        return phone_number,extension_numbers
+
+def send_sms(phone_number:str, message:str,sender_name:str="send-Test"):
+    """SMSメッセージを送信する
+
+    Args:
+        phone_number (str): 国際電話番号(+00 11 2222 3333)
+        message (str): 本文
+        sender_name (str, optional): 送信者の名前.英数字と-のみ使用可能. Defaults to "send-Test".
+
+    Returns:
+        bool: メッセージ送信完了
+    
+    Raises:
+        Exception:何らかのエラーが発生した場合
+    
+    Examples:
+        send_sms("+811122223333,"message","sender")
+    """
+    message_attributes = {
+    'AWS.SNS.SMS.SenderID': {
+    'DataType': 'String',
+    'StringValue': sender_name # 通知者表示に使用される送信者ID
+    }
+    }
+    
+
+    sns = boto3.client(
+        'sns', 
+        aws_access_key_id=AWS_ACCESS_KEY, 
+        aws_secret_access_key=AWS_SECRET_KEY, 
+        region_name='ap-northeast-1'
+    )
+    response = sns.publish(
+        PhoneNumber = phone_number,
+        Message = message,
+        MessageAttributes = message_attributes
+    )
+    return True
