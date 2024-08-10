e5db354c5ec11ee6ea9953895910e166893d6643
I.F <131729144+FukumotoIkuma@users.noreply.github.com>
2024-08-10 20:07:55 +0900

    fix:zoomへのレスポンスが遅く、webhookが繰り返し送られる問題

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request,jsonify,make_response
 from pprint import pformat
 from system import create_hmac_sha256
 import os
+import threading
 
 from send_sms import ZoomAPI,send_sms
 
@@ -46,17 +47,21 @@ def webhook():
         return res
 
     elif event == "phone.callee_ended":
-        call_id = payload["object"]["call_id"]
-        phone_number,extension_numbers = zoom_api.extract_SMS_info(call_id)
-        print(f"extension_numbers: {extension_numbers}")
+        def inner():
+            call_id = payload["object"]["call_id"]
+            phone_number,extension_numbers = zoom_api.extract_SMS_info(call_id)
+            print(f"extension_numbers: {extension_numbers}")
+            #TODO 本番環境で可変にすべき
+            if "210" in extension_numbers:
+                try:
+                    send_sms(phone_number=phone_number,message="test")
+                    print("completely sended msg")
+                except Exception as e:
+                    print(e)
+        # スレッドでSMS送信処理を行うのは、Zoomへのレスポンスを即座に行うため
+        threading.Thread(target=inner).start()
 
-        #TODO 本番環境で可変にすべき
-        if "210" in extension_numbers:
-            try:
-                send_sms(phone_number=phone_number,message="test")
-                print("completely sended msg")
-            except Exception as e:
-                print(e)
+        
 
     return make_response("",200)
 
